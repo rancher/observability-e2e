1641963713c5082d7de26c7b01e17896cbfc108c
Pankaj Kumar <pkumar@Pankajs-MacBook-Pro.local>
2024-11-13 13:16:14 +0530

    pkumar: adding test for create prometheus rule

--- tests/e2e/monitoring_e2e_test.go
+++ tests/e2e/monitoring_e2e_test.go
@@ -7,8 +7,13 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	"github.com/rancher/observability-e2e/tests/helper/charts"
 	rancher "github.com/rancher/shepherd/clients/rancher"
 	"github.com/rancher/shepherd/extensions/kubectl"
+	"github.com/rancher/shepherd/pkg/namegenerator"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 )
 
@@ -36,6 +41,11 @@ type AlertStatus struct {
 	SilencedBy  []string `json:"silencedBy"`
 	State       string   `json:"state"`
 }
+
+const defaultRandStringLength = 5
+const prometheusRulesSteveType = "monitoring.coreos.com.prometheusrule"
+
+var ruleLabel = map[string]string{"team": "qa"}
 
 var _ = Describe("Observability Installation Test Suite", func() {
 	var clientWithSession *rancher.Client //RancherConfig *Config
@@ -99,6 +109,51 @@ var _ = Describe("Observability Installation Test Suite", func() {
 				e2e.Logf("Verified container is deleted %v", deleteConfirm)
 			}
 		}()
+
+	})
+
+	It("Test : Verify Creating prometheus rule", Label("LEVEL1", "monitoring", "E2E"), func() {
+
+		ruleName := "webhook-rule-" + namegenerator.RandStringLower(defaultRandStringLength)
+		alertName := "alert-" + namegenerator.RandStringLower(defaultRandStringLength)
+
+		By("1) Client login")
+		_, err := client.ReLogin()
+		if err != nil {
+			e2e.Failf("Failed to relogin. Error: %v", err)
+		}
+		By("2) Get the steveclient for the local cluster ")
+		steveclient, err := client.Steve.ProxyDownstream("local") // Get the steveclient for the local cluster
+		if err != nil {
+			e2e.Failf("Error on steveclient: %v", err)
+		}
+
+		prometheusRule := &monitoringv1.PrometheusRule{ // Create the Prometheus Rule
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      ruleName,
+				Namespace: charts.RancherMonitoringNamespace,
+			},
+			Spec: monitoringv1.PrometheusRuleSpec{
+				Groups: []monitoringv1.RuleGroup{
+					{
+						Name: ruleName,
+						Rules: []monitoringv1.Rule{
+							{
+								Alert:  alertName,
+								Expr:   intstr.IntOrString{Type: intstr.String, StrVal: "vector(1)"},
+								Labels: ruleLabel,
+								For:    "0s",
+							},
+						},
+					},
+				},
+			},
+		}
+		By("3) Create the Prometheus Rule on local cluster ")
+		_, err = steveclient.SteveType(prometheusRulesSteveType).Create(prometheusRule)
+		if err != nil {
+			e2e.Failf("Error on creation of Prometheus Rule: %v", err)
+		}
 
 	})
 
