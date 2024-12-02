c4e1ca85079a96ae48ed78b17a1a50ec94cc7dba
Pankaj Kumar <pkumar@Pankajs-MacBook-Pro.local>
2024-12-02 15:45:00 +0530

    pkumar| refactoring test for create prometheue rule

---
+++ tests/helper/utils/utils.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"log"
+	"os"
+
+	rancher "github.com/rancher/shepherd/clients/rancher"
+	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	"github.com/rancher/shepherd/extensions/kubectl"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
+)
+
+func DeployPrometheusRule(mySession *rancher.Client, yamlPath string) error {
+
+	yamlContent, err := os.ReadFile(yamlPath)
+	if err != nil {
+		log.Fatalf("Failed to read file %s: %v", yamlPath, err)
+	}
+
+	importYamlInput := &management.ImportClusterYamlInput{
+		YAML: string(yamlContent),
+	}
+
+	apply := []string{"kubectl", "apply", "-f", "/root/.kube/my-pod.yaml"}
+	prometheusRuleApply, err := kubectl.Command(mySession, importYamlInput, "local", apply, "")
+	if err != nil {
+		return err
+	}
+	e2e.Logf("Successfully fetchall: %v", prometheusRuleApply)
+
+	return nil
+}
