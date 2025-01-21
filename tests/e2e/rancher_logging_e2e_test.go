3610001fbd4c1d0fc1c4ea1dce0d9236b3bdfd25
Pankaj <149133120+pankajsqe@users.noreply.github.com>
2025-01-21 13:10:11 +0530

    pkumar:refactroing rancher logging e2e test with goassertion (#43)
    Co-authored-by: Pankaj Kumar <pkumar@Pankajs-MacBook-Pro.local>

--- tests/e2e/rancher_logging_e2e_test.go
+++ tests/e2e/rancher_logging_e2e_test.go
@@ -1,7 +1,6 @@
 package e2e_test
 
 import (
-	"fmt"
 	"os"
 	"strings"
 	"time"
@@ -19,7 +18,7 @@ const (
 )
 
 var _ = Describe("Observability Logging E2E Test Suite", func() {
-	var clientWithSession *rancher.Client //RancherConfig *Config
+	var clientWithSession *rancher.Client // RancherConfig *Config
 
 	JustBeforeEach(func() {
 		By("Creating a client session")
@@ -32,9 +31,7 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 		By("0) Fetch all the deployments belonging to rancher-logging")
 		fetchDeployments := []string{"kubectl", "get", "deployments", "-n", "cattle-logging-system", "--no-headers"}
 		rancherLoggingDeployments, err := kubectl.Command(clientWithSession, nil, "local", fetchDeployments, "")
-		if err != nil {
-			e2e.Failf("Failed to get deployments. Error: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Failed to get deployments.")
 
 		By("1) Read all the deployments and verify the status of rancher-logging deployments")
 		foundRancherLogging := false
@@ -44,10 +41,8 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 				continue
 			}
 
-			fields := strings.Fields(deployment) // Split the line into deployment name and its current status
-			if len(fields) < 4 {
-				e2e.Failf("Unexpected output format for deployment: %s", deployment)
-			}
+			fields := strings.Fields(deployment)
+			Expect(len(fields)).To(BeNumerically(">=", 4), "Unexpected output format for deployment: %s", deployment)
 
 			deploymentName := fields[0]
 			readyReplicas := fields[1]
@@ -59,23 +54,11 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 			if strings.HasPrefix(deploymentName, "rancher-logging") {
 				foundRancherLogging = true
 
-				if availableReplicas == desiredCount {
-					e2e.Logf("Success: Deployment %s is fully available. Desired: %s, Available: %s", deploymentName, desiredCount, availableReplicas)
-				} else {
-					e2e.Failf("Failure: Deployment %s is not fully available. Desired: %s, Available: %s", deploymentName, desiredCount, availableReplicas)
-				}
-
-				if readyCount == desiredCount {
-					e2e.Logf("Success: Deployment %s pods are fully ready. Desired: %s, Ready: %s", deploymentName, desiredCount, readyCount)
-				} else {
-					e2e.Failf("Failure: Deployment %s pods are not fully ready. Desired: %s, Ready: %s", deploymentName, desiredCount, readyCount)
-				}
+				Expect(availableReplicas).To(Equal(desiredCount), "Failure: Deployment %s is not fully available. Desired: %s, Available: %s", deploymentName, desiredCount, availableReplicas)
+				Expect(readyCount).To(Equal(desiredCount), "Failure: Deployment %s pods are not fully ready. Desired: %s, Ready: %s", deploymentName, desiredCount, readyCount)
 			}
 		}
-		if !foundRancherLogging {
-			e2e.Failf("No deployments found starting with 'rancher-logging'")
-		}
-
+		Expect(foundRancherLogging).To(BeTrue(), "No deployments found starting with 'rancher-logging'")
 	})
 
 	It("Test : Verify status of rancher-logging pods using kubectl", Label("LEVEL1", "Logging", "E2E"), func() {
@@ -83,9 +66,7 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 		By("0) Fetch all the pods belongs to rancher-logging")
 		fetchPods := []string{"kubectl", "get", "pods", "-n", "cattle-logging-system", "--no-headers"}
 		rancherLoggingPods, err := kubectl.Command(clientWithSession, nil, "local", fetchPods, "")
-		if err != nil {
-			e2e.Failf("Failed to get pods . Error: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Failed to get pods.")
 
 		By("1) Read all the pods and verify the status of rancher-logging-Pods")
 		rancherLoggingPodFound := false
@@ -94,10 +75,8 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 			if pod == "" {
 				continue
 			}
-			fields := strings.Fields(pod) // Split the line into pod name and its current status
-			if len(fields) < 3 {
-				e2e.Failf("Unexpected output format for pod: %s", pod)
-			}
+			fields := strings.Fields(pod)
+			Expect(len(fields)).To(BeNumerically(">=", 3), "Unexpected output format for pod: %s", pod)
 
 			podName := fields[0]
 			podStatus := fields[2]
@@ -105,12 +84,8 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 			if strings.HasPrefix(podName, "rancher-logging") && (podStatus == "Running" || podStatus == "Completed") {
 				rancherLoggingPodFound = true
 			}
-
 		}
-		if !rancherLoggingPodFound {
-			e2e.Failf("Pod with name 'rancher-logging' is not running or not present")
-		}
-
+		Expect(rancherLoggingPodFound).To(BeTrue(), "Pod with name 'rancher-logging' is not running or not present")
 	})
 
 	It("Test : Verify status of rancher-logging DaemonSets using kubectl", Label("LEVEL1", "Logging", "E2E"), func() {
@@ -118,10 +93,7 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 		By("0) Fetch all the daemon sets belongs to rancher-logging")
 		fetchDaemonSets := []string{"kubectl", "get", "daemonsets", "-n", "cattle-logging-system", "--no-headers"}
 		rancherLoggingDaemonSets, err := kubectl.Command(clientWithSession, nil, "local", fetchDaemonSets, "")
-		if err != nil {
-			e2e.Failf("Failed to get daemonsets. Error: %v", err)
-		}
-		e2e.Logf("Deamonset Fetched succsefully %v", rancherLoggingDaemonSets)
+		Expect(err).NotTo(HaveOccurred(), "Failed to get daemonsets.")
 
 		By("1) Read all the daemonSet and verify the status of rancher-logging-daemonSets")
 		daemonSets := strings.Split(rancherLoggingDaemonSets, "\n")
@@ -130,25 +102,17 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 				continue
 			}
 
-			fields := strings.Fields(daemonSet) // Split the line into pod name and its current status
-			if len(fields) < 6 {
-				e2e.Failf("Unexpected output format for daemonSet: %s", daemonSet)
-			}
+			fields := strings.Fields(daemonSet)
+			Expect(len(fields)).To(BeNumerically(">=", 6), "Unexpected output format for daemonSet: %s", daemonSet)
 
 			daemonSetName := fields[0]
 			desiredPods := fields[1]
 			readyPods := fields[3]
 			availablePods := fields[5]
 
-			if desiredPods != availablePods {
-				e2e.Failf("DaemonSet %s is not fully available. Desired: %s, Available: %s", daemonSetName, desiredPods, availablePods)
-			}
-
-			if readyPods != desiredPods {
-				e2e.Failf("DaemonSet %s is not fully ready. Desired: %s, Ready: %s", daemonSetName, desiredPods, readyPods)
-			}
+			Expect(availablePods).To(Equal(desiredPods), "DaemonSet %s is not fully available. Desired: %s, Available: %s", daemonSetName, desiredPods, availablePods)
+			Expect(readyPods).To(Equal(desiredPods), "DaemonSet %s is not fully ready. Desired: %s, Ready: %s", daemonSetName, desiredPods, readyPods)
 		}
-
 	})
 
 	It("Test : Verify status of rancher-logging StatefulSets using kubectl", Label("LEVEL1", "Logging", "E2E"), func() {
@@ -156,9 +120,7 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 		By("0) Fetch all the StatefulSets belongs to rancher-logging")
 		fetchStatefulsets := []string{"kubectl", "get", "statefulsets", "-n", "cattle-logging-system", "--no-headers"}
 		rancherLoggingStatefulsets, err := kubectl.Command(clientWithSession, nil, "local", fetchStatefulsets, "")
-		if err != nil {
-			e2e.Failf("Failed to get daemonsets . Error: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Failed to get statefulsets.")
 
 		By("1) Read all the statefulsets and verify the status of rancher-logging-statefulsets")
 		statefulsets := strings.Split(rancherLoggingStatefulsets, "\n")
@@ -167,10 +129,8 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 				continue
 			}
 
-			fields := strings.Fields(statefulset) // Split the line into pod name and its current status
-			if len(fields) < 3 {
-				e2e.Failf("Unexpected output format for daemonSet: %s", statefulset)
-			}
+			fields := strings.Fields(statefulset)
+			Expect(len(fields)).To(BeNumerically(">=", 3), "Unexpected output format for statefulSet: %s", statefulset)
 
 			statefulsetsName := fields[0]
 			readyReplicas := fields[1]
@@ -179,16 +139,9 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 			desiredCount := strings.Split(readyReplicas, "/")[1]
 
 			if strings.HasPrefix(statefulsetsName, "rancher-logging") {
-
-				if readyCount == desiredCount {
-					e2e.Logf("Success: Deployment %s pods are fully ready. Desired: %s, Ready: %s", statefulsetsName, desiredCount, readyCount)
-				} else {
-					e2e.Failf("Failure: Deployment %s pods are not fully ready. Desired: %s, Ready: %s", statefulsetsName, desiredCount, readyCount)
-				}
+				Expect(readyCount).To(Equal(desiredCount), "Failure: Deployment %s pods are not fully ready. Desired: %s, Ready: %s", statefulsetsName, desiredCount, readyCount)
 			}
-
 		}
-
 	})
 
 	It("Test: Verify creation of Rancher cluster output and cluster flow", Label("LEVEL1", "Logging", "E2E"), func() {
@@ -196,67 +149,49 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 		By("1) Fetching syslog service IP for cluster output host")
 		syslogServiceCmd := []string{"kubectl", "get", "svc", "syslog-ng-service", "-n", "cattle-logging-system", "--no-headers"}
 		syslogServiceOutput, err := kubectl.Command(clientWithSession, nil, "local", syslogServiceCmd, "")
-		if err != nil {
-			e2e.Failf("Failed to get syslog service. Error: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Failed to get syslog service")
+
 		serviceFields := strings.Fields(syslogServiceOutput)
 		var serviceIP string
-		if len(serviceFields) > 2 {
-			serviceIP = serviceFields[2]
-			e2e.Logf("Syslog service IP: %s", serviceIP)
-		} else {
-			e2e.Failf("Failed to extract service IP from syslog service output")
-		}
+		Expect(len(serviceFields)).To(BeNumerically(">", 2), "Failed to extract service IP from syslog service output")
+		serviceIP = serviceFields[2]
+		e2e.Logf("Syslog service IP: %s", serviceIP)
 
 		By("2) Updating cluster flow and output manifest file")
 		yamlContent, err := os.ReadFile(loggingResourceYamlPath)
-		if err != nil {
-			e2e.Failf("Error reading YAML file: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Error reading YAML file")
 
 		yamlStr := string(yamlContent)
 		updatedYamlStr := strings.Replace(yamlStr, "<syslog service IP>", serviceIP, -1)
 
 		err = os.WriteFile(loggingResourceYamlPath, []byte(updatedYamlStr), 0644)
-		if err != nil {
-			e2e.Failf("Error writing updated YAML file: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Error writing updated YAML file")
 
 		By("3) Deploying cluster output and cluster flow")
 		deployLoggingResourcesError := utils.DeployLoggingClusterOutputAndClusterFlow(clientWithSession, loggingResourceYamlPath)
-		if deployLoggingResourcesError != nil {
-			e2e.Failf("Failed to deploy cluster output and flow: %v", deployLoggingResourcesError)
-		}
+		Expect(deployLoggingResourcesError).NotTo(HaveOccurred(), "Failed to deploy cluster output and flow")
 
 		By("4) Fetching cluster output")
 		clusterOutputCmd := []string{"kubectl", "get", "clusteroutput", "testclusteroutput", "-n", "cattle-logging-system"}
 		clusterOutput, err := kubectl.Command(clientWithSession, nil, "local", clusterOutputCmd, "")
-		if err != nil {
-			e2e.Failf("Failed to fetch cluster output. Error: %v", err)
-		}
-		e2e.Logf("Successfully fetched cluster output:\n %v", clusterOutput)
+		Expect(err).NotTo(HaveOccurred(), "Failed to fetch cluster output")
+		Expect(clusterOutput).NotTo(BeEmpty(), "Cluster output is empty")
 
 		By("5) Fetching cluster flow")
 		clusterFlowCmd := []string{"kubectl", "get", "clusterflow", "testclusterflow", "-n", "cattle-logging-system"}
 		clusterFlow, err := kubectl.Command(clientWithSession, nil, "local", clusterFlowCmd, "")
-		if err != nil {
-			e2e.Failf("Failed to fetch cluster flow. Error: %v", err)
-		}
-		e2e.Logf("Successfully fetched cluster flow:\n %v", clusterFlow)
+		Expect(err).NotTo(HaveOccurred(), "Failed to fetch cluster flow")
+		Expect(clusterFlow).NotTo(BeEmpty(), "Cluster flow is empty")
 
 		By("6) Scaling Rancher Logging Deployment")
 		scaleRancherLogging := []string{"kubectl", "scale", "deployment", "rancher-logging", "-n", "cattle-logging-system", "--replicas=4"}
 		fetchRancherLogging := []string{"kubectl", "get", "pods", "-n", "cattle-logging-system"}
 		scaleDeploymentOutput, err := kubectl.Command(clientWithSession, nil, "local", scaleRancherLogging, "")
-		if err != nil {
-			e2e.Failf("Failed to scale Rancher logging deployment. Error: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Failed to scale Rancher logging deployment")
 		e2e.Logf("Successfully scaled Rancher logging deployment:\n %v", scaleDeploymentOutput)
 
 		fetchRancherLoggingOutput, err := kubectl.Command(clientWithSession, nil, "local", fetchRancherLogging, "")
-		if err != nil {
-			e2e.Failf("Failed to fetch Rancher logging pods. Error: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Failed to fetch Rancher logging pods")
 		e2e.Logf("Successfully fetched Rancher logging pods:\n %v", fetchRancherLoggingOutput)
 
 		var maxRetries = 5
@@ -267,57 +202,54 @@ var _ = Describe("Observability Logging E2E Test Suite", func() {
 		time.Sleep(30 * time.Second)
 		syslogPodsCmd := []string{"kubectl", "get", "pods", "-n", "cattle-logging-system", "--no-headers"}
 		syslogPodsOutput, err := kubectl.Command(clientWithSession, nil, "local", syslogPodsCmd, "")
-		if err != nil {
-			e2e.Failf("Failed to fetch syslog pods. Error: %v", err)
-		}
+		Expect(err).NotTo(HaveOccurred(), "Failed to fetch syslog pods")
 
 		syslogPods := strings.Split(syslogPodsOutput, "\n")
+		Expect(syslogPods).NotTo(BeEmpty(), "No syslog pods found")
+
 		for _, pod := range syslogPods {
 			if strings.Contains(pod, "syslog-ng-deployment") {
 				podFields := strings.Fields(pod)
-				if len(podFields) > 0 {
-					podName := podFields[0]
-					e2e.Logf("Fetching logs for syslog pod: %v", podName)
-					var syslogLogsOutput string
-					var err error
+				Expect(len(podFields)).To(BeNumerically(">", 0), "Invalid pod format")
 
-					// Retries
-					for attempt < maxRetries {
-						syslogLogsCmd := []string{"kubectl", "logs", podName, "-n", "cattle-logging-system"}
-						syslogLogsOutput, err = kubectl.Command(clientWithSession, nil, "local", syslogLogsCmd, "")
-						if err != nil {
-							e2e.Failf("Error fetching logs for pod %s: %v", podName, err)
+				podName := podFields[0]
+				e2e.Logf("Fetching logs for syslog pod: %v", podName)
+				var syslogLogsOutput string
+				var err error
+
+				for attempt < maxRetries {
+					syslogLogsCmd := []string{"kubectl", "logs", podName, "-n", "cattle-logging-system"}
+					syslogLogsOutput, err = kubectl.Command(clientWithSession, nil, "local", syslogLogsCmd, "")
+					Expect(err).NotTo(HaveOccurred(), "Error fetching logs for pod %s", podName)
+
+					if syslogLogsOutput == "" || syslogLogsOutput == "null" {
+						e2e.Logf("Logs are empty or null. Retrying... (Attempt %d/%d)\n", attempt+1, maxRetries)
+						attempt++
+						if attempt < maxRetries {
+							time.Sleep(retryInterval)
 						}
-
-						// If logs are empty or nil, retry
-						if syslogLogsOutput == "" || syslogLogsOutput == "null" {
-							e2e.Logf("Logs are empty or null. Retrying... (Attempt %d/5)\n", attempt+1)
-							attempt++
-							if attempt < 5 {
-								time.Sleep(retryInterval)
-							}
-						} else {
-							logLines := strings.Split(syslogLogsOutput, "\n")
-							startIdx := max(0, len(logLines)-5)
-							e2e.Logf("Syslog found :\n%s", strings.Join(logLines[startIdx:], "\n"))
-							break
-						}
+					} else {
+						logLines := strings.Split(syslogLogsOutput, "\n")
+						startIdx := max(0, len(logLines)-5)
+						e2e.Logf("Syslog found:\n%s", strings.Join(logLines[startIdx:], "\n"))
+						break
 					}
-
-					if attempt == maxRetries && !strings.Contains(syslogLogsOutput, "testclusteroutput") {
-						e2e.Failf("Logs for pod %s did not contain 'testclusteroutput' after %d attempts", podName, maxRetries)
-					}
-					attempt = 0
 				}
+
+				Expect(attempt).Should(BeNumerically("<", maxRetries), "Failed to fetch valid logs for pod after max retries")
+				Expect(syslogLogsOutput).To(
+					Or(
+						ContainSubstring("testclusteroutput"), ContainSubstring("cattle-logging-system"), ContainSubstring("syslog-ng"),
+					),
+					"Logs for pod %s did not contain any of the expected substrings: 'testclusteroutput', 'cattle-logging-system', or 'cattle-monitoring-system'", podName)
+
+				attempt = 0
 			}
 		}
-		resetOriginalYaml := strings.Replace(yamlStr, serviceIP, "<syslog service IP>", -1)
 
+		resetOriginalYaml := strings.Replace(yamlStr, serviceIP, "<syslog service IP>", -1)
 		err = os.WriteFile(loggingResourceYamlPath, []byte(resetOriginalYaml), 0644)
-		if err != nil {
-			fmt.Printf("Error writing updated YAML file: %v\n", err)
-			return
-		}
+		Expect(err).NotTo(HaveOccurred(), "Error writing reset YAML file")
 	})
 
 })
