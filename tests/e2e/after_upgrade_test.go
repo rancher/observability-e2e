4ac51b0e11fb8146c27e6a6cd0df48df79681308
Deepak Punia <deepak.punia@suse.com>
2025-04-25 15:50:06 +0530

    Added Observability monitoring chart upgrade

---
+++ tests/e2e/after_upgrade_test.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2024 - 2025 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/rancher/observability-e2e/tests/helper/charts"
+	rancher "github.com/rancher/shepherd/clients/rancher"
+	catalog "github.com/rancher/shepherd/clients/rancher/catalog"
+	extencharts "github.com/rancher/shepherd/extensions/charts"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
+)
+
+var _ = Describe("Observability Upgrade Test Suite", func() {
+	var clientWithSession *rancher.Client
+	var err error
+
+	JustBeforeEach(func() {
+		By("Creating a client session")
+		clientWithSession, err = client.WithSession(sess)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("[QASE-3891] Upgrade monitoring chart to the Latest Version", Label("monitoring", "afterUpgrade"), func() {
+		testCaseID = 3891
+		By("Checking if the monitoring chart is already installed")
+		initialMonitoringChart, err := extencharts.GetChartStatus(clientWithSession, project.ClusterID, charts.RancherMonitoringNamespace, charts.RancherMonitoringName)
+		Expect(err).NotTo(HaveOccurred())
+
+		if initialMonitoringChart.IsAlreadyInstalled {
+			e2e.Logf("Monitoring chart is already installed in project: %v", exampleAppProjectName)
+			e2e.Logf("Getting Monitoring Newer Version")
+			monitoringVersionChartList, err := clientWithSession.Catalog.GetListChartVersions(charts.RancherMonitoringName, catalog.RancherChartRepo)
+			Expect(err).NotTo(HaveOccurred())
+			e2e.Logf("Chart List: %v", monitoringVersionChartList)
+			if len(monitoringVersionChartList) == 0 {
+				Skip("No newer versions found for the monitoring chart to perform the upgrade")
+			}
+			latestMonitoringVersion := monitoringVersionChartList[0]
+
+			chartInstallOptions := &charts.InstallOptions{
+				Cluster:   cluster,
+				Version:   latestMonitoringVersion,
+				ProjectID: project.ID,
+			}
+			chartFeatureOptions := &charts.RancherMonitoringOpts{
+				IngressNginx:      true,
+				ControllerManager: true,
+				Etcd:              true,
+				Proxy:             true,
+				Scheduler:         true,
+			}
+			e2e.Logf("Retrieved latest monitoring chart version to install: %v", latestMonitoringVersion)
+
+			By("Upgrading monitoring chart to the latest version")
+			err = charts.UpgradeRancherMonitoringChart(clientWithSession, chartInstallOptions, chartFeatureOptions)
+			if err != nil {
+				e2e.Failf("Failed to upgrade the monitoring chart. Error: %v", err)
+			}
+		} else {
+			Skip("Monitoring is not installed. Execute the pre-upgrade installation test before attempting the upgrade")
+		}
+	})
+})
--- tests/helper/charts/ranchermonitoring.go
+++ tests/helper/charts/ranchermonitoring.go
@@ -3,6 +3,7 @@ package charts
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -161,5 +162,146 @@ func InstallRancherMonitoringChart(client *rancher.Client, installOptions *Insta
 	}
 
 	// The app has been successfully deployed.
+	return nil
+}
+
+func UpgradeRancherMonitoringChart(client *rancher.Client, installOptions *InstallOptions, rancherMonitoringOpts *RancherMonitoringOpts) error {
+	// Retrieve server settings with scheme validation
+	serverSetting, err := client.Management.Setting.ByID(serverURLSettingID)
+	if err != nil {
+		return err
+	}
+	serverURL := serverSetting.Value
+	if !strings.HasPrefix(serverURL, "http://") && !strings.HasPrefix(serverURL, "https://") {
+		serverURL = "https://" + serverURL
+	}
+
+	// Retrieve registry settings
+	registrySetting, err := client.Management.Setting.ByID(defaultRegistrySettingID)
+	if err != nil {
+		return err
+	}
+
+	// Base monitoring values configuration
+	monitoringValues := map[string]interface{}{
+		"prometheus": map[string]interface{}{
+			"prometheusSpec": map[string]interface{}{
+				"evaluationInterval": "1m",
+				"retentionSize":      "50GiB",
+				"scrapeInterval":     "1m",
+			},
+		},
+	}
+
+	// Process monitoring options with provider-specific prefixes
+	optsBytes, err := json.Marshal(rancherMonitoringOpts)
+	if err != nil {
+		return err
+	}
+	optsMap := make(map[string]interface{})
+	if err := json.Unmarshal(optsBytes, &optsMap); err != nil {
+		return err
+	}
+
+	for key, value := range optsMap {
+		if key == "ingressNginx" && installOptions.Cluster.Provider == clusters.KubernetesProviderRKE {
+			monitoringValues[key] = map[string]interface{}{"enabled": value}
+			continue
+		}
+		prefixedKey := fmt.Sprintf("%v%v%v",
+			installOptions.Cluster.Provider,
+			strings.ToUpper(string(key[0])),
+			key[1:],
+		)
+		monitoringValues[prefixedKey] = map[string]interface{}{"enabled": value}
+	}
+
+	// Create chart upgrade actions
+	chartUpgrade := newChartUpgrade(
+		RancherMonitoringName,
+		RancherMonitoringName,
+		installOptions.Version,
+		installOptions.Cluster.ID,
+		installOptions.Cluster.Name,
+		serverURL,
+		registrySetting.Value,
+		monitoringValues,
+	)
+	chartUpgradeCRD := newChartUpgrade(
+		RancherMonitoringCRDName,
+		RancherMonitoringCRDName,
+		installOptions.Version,
+		installOptions.Cluster.ID,
+		installOptions.Cluster.Name,
+		serverURL,
+		registrySetting.Value,
+		nil, // CRD typically doesn't need values
+	)
+	chartUpgrades := []types.ChartUpgrade{*chartUpgradeCRD, *chartUpgrade}
+	chartUpgradeAction := newChartUpgradeAction(RancherMonitoringNamespace, chartUpgrades)
+
+	// Execute chart upgrade
+	catalogClient, err := client.GetClusterCatalogClient(installOptions.Cluster.ID)
+	if err != nil {
+		return err
+	}
+	if err := catalogClient.UpgradeChart(chartUpgradeAction, catalog.RancherChartRepo); err != nil {
+		return err
+	}
+
+	// Setup watch with timeout
+	timeoutSeconds := int64(5 * 60) // 5 minute timeout
+	adminClient, err := rancher.NewClient(client.RancherConfig.AdminToken, client.Session)
+	if err != nil {
+		return err
+	}
+	adminCatalogClient, err := adminClient.GetClusterCatalogClient(installOptions.Cluster.ID)
+	if err != nil {
+		return err
+	}
+
+	// Watch for upgrade completion in two phases
+	for _, phase := range []struct {
+		targetState string
+		phaseName   string
+	}{
+		{string(catalogv1.StatusPendingUpgrade), "pending upgrade"},
+		{string(catalogv1.StatusDeployed), "deployed"},
+	} {
+		watchInterface, err := adminCatalogClient.Apps(RancherMonitoringNamespace).Watch(
+			context.TODO(),
+			metav1.ListOptions{
+				FieldSelector:  "metadata.name=" + RancherMonitoringName,
+				TimeoutSeconds: &timeoutSeconds,
+			},
+		)
+		if err != nil {
+			return err
+		}
+
+		err = wait.WatchWait(watchInterface, func(event watch.Event) (bool, error) {
+			app, ok := event.Object.(*catalogv1.App)
+			if !ok {
+				return false, fmt.Errorf("unexpected type %T", event.Object)
+			}
+
+			switch app.Status.Summary.State {
+			case phase.targetState:
+				return true, nil
+			case string(catalogv1.StatusFailed):
+				return false, fmt.Errorf("upgrade failed at %s phase", phase.phaseName)
+			default:
+				return false, nil
+			}
+		})
+
+		if err != nil {
+			if errors.Is(err, context.DeadlineExceeded) {
+				return fmt.Errorf("timeout waiting for %s state (5 minutes)", phase.phaseName)
+			}
+			return err
+		}
+	}
+
 	return nil
 }
