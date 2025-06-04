5cb0b6a579853587a0308c6f4b9b99682aa227c8
Deepak Punia SUSE <deepak.punia@suse.com>
2025-06-04 17:20:46 +0530

    Added automation for logging chart upgrade scenarios (#71)

--- tests/e2e/before_upgrade_test.go
+++ tests/e2e/before_upgrade_test.go
@@ -36,7 +36,7 @@ var _ = Describe("Observability Upgrade Test Suite", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	It("Uninstall Existing Observability Charts", Label("monitoring", "beforeUpgrade"), func() {
+	It("Uninstall Existing Observability Charts", Label("beforeUpgrade"), func() {
 		chartsToUninstall := []struct {
 			name      string
 			namespace string
@@ -44,6 +44,8 @@ var _ = Describe("Observability Upgrade Test Suite", func() {
 			{name: charts.RancherMonitoringName, namespace: charts.RancherMonitoringNamespace},
 			{name: charts.RancherMonitoringCRDName, namespace: charts.RancherMonitoringNamespace},
 			{name: charts.PrometheusFederatorName, namespace: charts.PrometheusFederatorNamespace},
+			{name: charts.RancherLoggingName, namespace: charts.RancherLoggingNamespace},
+			{name: charts.RancherLoggingCRDName, namespace: charts.RancherLoggingNamespace},
 		}
 
 		for _, chartInfo := range chartsToUninstall {
@@ -59,7 +61,7 @@ var _ = Describe("Observability Upgrade Test Suite", func() {
 		}
 	})
 
-	It("[QASE-3889] Install monitoring chart One Older Version", Label("monitoring", "beforeUpgrade"), func() {
+	It("[QASE-3889] Install an older version of the Monitoring Chart", Label("monitoring", "beforeUpgrade"), func() {
 		testCaseID = 3889
 		e2e.Logf("Getting Monitoring Older Version")
 		monitoringVersionChartList, err := clientWithSession.Catalog.GetListChartVersions(charts.RancherMonitoringName, catalog.RancherChartRepo)
@@ -93,7 +95,7 @@ var _ = Describe("Observability Upgrade Test Suite", func() {
 		}
 	})
 
-	It("[QASE-8321] Install prometheus federator chart One Older Version", Label("promfed", "beforeUpgrade"), func() {
+	It("[QASE-8321] Install an older version of the prometheus federator chart", Label("promfed", "beforeUpgrade"), func() {
 		testCaseID = 8321
 		e2e.Logf("Getting prometheus federator Older Version")
 		promFedChartList, err := clientWithSession.Catalog.GetListChartVersions(charts.PrometheusFederatorName, catalog.RancherChartRepo)
@@ -124,4 +126,37 @@ var _ = Describe("Observability Upgrade Test Suite", func() {
 			e2e.Failf("Failed to install the prometheus federator chart. Error: %v", err)
 		}
 	})
+
+	It("[QASE-3896] Install an older version of the Logging chart", Label("logging", "beforeUpgrade"), func() {
+		testCaseID = 3896
+
+		e2e.Logf("Getting Logging Older Version")
+		LoggingVersionChartList, err := clientWithSession.Catalog.GetListChartVersions(charts.RancherLoggingName, catalog.RancherChartRepo)
+		Expect(err).NotTo(HaveOccurred())
+		e2e.Logf("Chart List: %v", LoggingVersionChartList)
+
+		if len(LoggingVersionChartList) <= 1 {
+			Skip("Not enough older versions found for the Logging chart to proceed with installation")
+		}
+		loggingVersion := LoggingVersionChartList[1]
+
+		loggingInstOpts := &charts.InstallOptions{
+			Cluster:   cluster,
+			Version:   loggingVersion,
+			ProjectID: project.ID,
+		}
+
+		loggingOpts := &charts.RancherLoggingOpts{
+			AdditionalLoggingSources: true,
+		}
+
+		e2e.Logf("Retrieved logging chart version to install: %v", loggingVersion)
+
+		By(fmt.Sprintf("Installing logging chart with an %v version", loggingVersion))
+		err = charts.InstallRancherLoggingChart(clientWithSession, loggingInstOpts, loggingOpts)
+		if err != nil {
+			e2e.Failf("Failed to install the logging chart. Error: %v", err)
+		}
+	})
+
 })
